56621dfc31530f2cc6d227a8ce3c385193aa7de1
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-02-07 09:14:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Clean String
+Permet de nettoyer un fichier .csv
+
+## Fonctionnement
+* Ajouter le fichier à nettoyer dans le dossier courant
+* Le renommer manuellement dirty.csv
+* Déclencher le nettoyage
+  * Ouvrir un terminal
+  * Taper `node clean.js`
+* Un fichier clean.csv avec le résultat sera créé
+  
+  **Si le fichier le fichier est déjà créé le résultat va s'ajouter à la suite**
---
+++ clean.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+
+function cleanString(dirtyString) {
+  dirtyString = dirtyString.trim();
+  dirtyString = dirtyString.replace(/ /g, "-");
+  dirtyString = dirtyString.replace(/--/g, "-");
+  dirtyString = dirtyString.toLowerCase();
+  dirtyString =  dirtyString.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return dirtyString;
+}
+
+function processLineByLine() {
+    const fileStream = fs.createReadStream('dirty.csv');
+
+    const rl = readline.createInterface({
+      input: fileStream,
+    });
+  
+    rl.on('line', (line) => {
+      let newLine = cleanString(line);
+      fs.appendFileSync("clean.csv", `${newLine}\n`, "utf8");
+    });
+  }
+
+processLineByLine();
